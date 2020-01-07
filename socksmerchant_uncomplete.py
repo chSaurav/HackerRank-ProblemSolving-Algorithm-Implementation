77b3113c5b88018c65072528ae4cc33630ac8ed7
chSaurav <48649257+chSaurav@users.noreply.github.com>
2020-01-08 01:22:05 +0530

    Create socksmerchant_uncomplete.py

---
+++ socksmerchant_uncomplete.py
@@ -0,0 +1,18 @@
+#function
+def socksMerchant(socks):
+    totalPair = 0
+    for val in socks :
+        total = 0 
+        for i in range(0,len(socks)):
+                if val == socks[i]:
+                    total = total + 1
+        pair = int(total / 2)
+    totalPair += pair
+    return (totalPair)
+
+
+#driverCode
+n  = int(input("")) #number of socks in the pile
+socks = list(map(int, input().split(" ")))
+result = socksMerchant(socks)
+print(result)
